a13956f41b2d825420c406a6656459400c3a58bd
sandeepsingh84 <sandeep.bunty28@yahoo.com>
2021-04-06 19:22:26 +0530

    Axios get put delete api call

---
+++ src/App.js
@@ -0,0 +1,46 @@
+// import logo from "./logo.svg";
+import './App.css';
+// import Table from "./Table";
+// import Buttonstate from "./Buttonstate";
+// import Gridpic from "./Gridpics";
+// import Inputfields from "./Inputfields";
+// import Marketingpg from "./Marketingpg";
+// import Pricingpln from "./Pricingpln";
+// import Featurepricing from "./Featurepricing";
+// import Scrollover from "./Scrollover";
+// import Headermarket from "./Headermarket";
+// import Todolist from "./Todolist";
+// import HookStateEffect from "./HookStateEffect";
+// import TableHook from "./TableHook";
+// import ContactFormParent from "./contactform/ContactFormParent";
+// import ShoppingListParent from "./contactform/ShoppingListParent";
+// import ShoopingCart from "./ShoopingCart";
+// import ApiCalling from "./ApiCalling";
+// import TitleApi from "./ApiCalling/TitleApi";
+// import TodoApi from "./ApiCalling/TodoApi";
+// import PhotosApi from "./ApiCalling/PhotosApi";
+// import QueryApi from "./ApiCalling/QueryApi";
+// import PostApi from "./ApiCalling/PostApi";
+// import PostSuccessful from "./ApiCalling/PostSuccessful";
+import ApiPutDelete from './ApiPutDelete.jsx';
+import JavaScriptPractice from './JavaScriptPractice';
+function App() {
+  return (
+    <div className=''>
+      {/* <TableHook /> */}
+      {/* <ShoopingCart /> */}
+      {/* <ShoppingListParent /> */}
+      {/* <ApiCalling /> */}
+      {/* <TitleApi /> */}
+      {/* <TodoApi /> */}
+      {/* <PhotosApi/> */}
+      {/* <QueryApi /> */}
+      {/* <PostApi/> */}
+      {/* <PostSuccessful /> */}
+      {/* <ApiPutDelete /> */}
+      <JavaScriptPractice />
+    </div>
+  );
+}
+
+export default App;
